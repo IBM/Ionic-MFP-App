7f92abe19b6abcf51d4901d871f84dffd662c51d
Shiva Kumar H R <shivahr@in.ibm.com>
2017-12-20 17:26:05 +0530

    BUg fixes

--- IonicMobileApp/src/pages/report-new/report-new.ts
+++ IonicMobileApp/src/pages/report-new/report-new.ts
@@ -44,7 +44,8 @@ export class ReportNewPage {
       quality: 50, // picture quality
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
     }
     this.camera.getPicture(options) .then((imageData) => {
         this.base64Image = "data:image/jpeg;base64," + imageData;
